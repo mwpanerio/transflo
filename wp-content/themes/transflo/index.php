9c2298db3e7bfcd26c242942d047cf9187e178d4
Mark Win Panerio <mwpanerio@webfx.com>
2022-10-31 23:59:13 +0800

    Initializing resources page

--- wp-content/themes/transflo/index.php
+++ wp-content/themes/transflo/index.php
@@ -2,6 +2,39 @@
 
 <?php get_template_part('partials/masthead'); ?>
 
+<section class="subscribes section-padding hard-bottom">
+    <div class="container">
+        <div class="subscribe">
+            <h3 class="text-center">Subscribe to Our Newsletter</h3>
+            <div class="subscribe__wrap">
+                <div class="subscribe__box">
+                    <div class="form-col">
+                        <input type="text" placeholder="&nbsp;">
+                        <label>First Name <sup>*</sup></label>
+                    </div>
+                </div>
+                <div class="subscribe__box">
+                    <div class="form-col">
+                        <input type="text" placeholder="&nbsp;">
+                        <label>Last Name <sup>*</sup></label>
+                    </div>
+                </div>
+                <div class="subscribe__box subscribe__box--big">
+                    <div class="form-col">
+                        <input type="email" placeholder="&nbsp;">
+                        <label>Email Address <sup>*</sup></label>
+                    </div>
+                </div>
+                <div class="subscribe__button">
+                    <div class="form-col">
+                        <button type="submit" class="btn btn-primary">Submit <i class="icon-button-right"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 <?php $pagination = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1; ?>
 <?php if ( 1 === $pagination ) : // Only show Featured post on first page of posts ?>
     <?php
@@ -14,29 +47,50 @@
         );
     ?>
     <?php if ( $featured_post_query->have_posts() ) : while ( $featured_post_query->have_posts() ) : $featured_post_query->the_post(); ?>
-        <!-- featured post HTML goes here -->
+        <div class="blog-featured section-padding hard-bottom">
+            <div class="container">
+                <article class="blog-featured__card">
+                    <div class="blog-featured__image">
+                        <?php echo fx_get_image_tag(get_field('featured_image')); ?>
+                    </div>
+                    <div class="blog-featured__content">
+                        <div class="blog-featured__date"><?php echo get_the_date('F j, Y'); ?></div>
+                        <h2 class="h3"><?php echo get_the_title(); ?></h2>
+                        <?php
+                            $blocks = parse_blocks( $post->post_content );
+
+                            foreach ( $blocks as $block ) {
+                            if ( 'acf/wysiwyg' === $block['blockName'] ) {
+                                    if(isset($block["attrs"]["data"]["content"])) {
+                                        $excerpt = strip_tags(trim($block["attrs"]["data"]["content"]));
+                                    }
+                                    break;
+                                }
+                            }
+                        ?>
+                        <p><?php echo $excerpt; ?></p>
+                        <a href="<?php echo get_the_permalink(); ?>" class="btn btn-primary">Read More</a>
+                    </div>
+                </article>
+            </div>
+        </div>
     <?php endwhile; endif; wp_reset_postdata(); ?>
 <?php endif; ?>
 
 <?php if( have_posts() ): ?>
     <section class="blog-listing-container js-load-more-block section-margins">
         <div class="container">
-            <div class="row">
-                <div class="col-md-8">
-                    <div class="blog-listing js-load-more-posts">    
-                        <?php while( have_posts() ): the_post(); ?>
-                            <?php get_template_part( 'partials/loop-content' ); ?>
-                        <?php endwhile; ?>
-                    </div>
-                    <div class="blog-listing__pagination">
-                        <div class="col-xxs-12">
-                            <?php get_template_part( 'partials/pagination' ); ?> 
-                        </div>
+            <div class="blog-lising__wrapper">
+                <div class="blog-listing row cards-flex js-load-more-posts">
+                    <?php while( have_posts() ): the_post(); ?>
+                        <?php get_template_part( 'partials/loop-content' ); ?>
+                    <?php endwhile; ?>
+                </div>
+                <div class="blog-listing__pagination">
+                    <div class="col-xxs-12">
+                        <?php get_template_part( 'partials/pagination' ); ?> 
                     </div>
                 </div>
-                <div class="col-md-3 col-md-offset-1">
-                    <?php get_sidebar(); ?>
-                </div>             
             </div>
         </div>
     </section>

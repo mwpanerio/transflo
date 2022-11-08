4619b713bdd2394982113ab1a14ee48fe733ded3
Mark Win Panerio <mwpanerio@webfx.com>
2022-11-08 22:09:50 +0800

    Hotfix: Update site search animation

--- wp-content/themes/transflo/assets/js/global.js
+++ wp-content/themes/transflo/assets/js/global.js
@@ -146,7 +146,7 @@ var FX = ( function( FX, $ ) {
 			// nav search toggle
 			$('.js-search-toggle').on('click', () => {
 				$('.desktop-menu__phone, .js-search-toggle').toggleClass('js-search-active');
-				$('.desktop-menu__search').stop().fadeToggle();
+				$('.desktop-menu__search').stop().fadeToggle().toggleClass('is-shown');
                 $('.desktop-menu__search input[name="s"]').focus();
 			});
 

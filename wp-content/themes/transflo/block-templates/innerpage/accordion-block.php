400580be748577a3ed67081970697900fddb34ec
Mark Win Panerio <mwpanerio@webfx.com>
2022-11-09 22:16:22 +0800

    Added custom field for accordion

--- wp-content/themes/transflo/block-templates/innerpage/accordion-block.php
+++ wp-content/themes/transflo/block-templates/innerpage/accordion-block.php
@@ -1,32 +1,30 @@
 <section class="accordion-block section-padding">
     <div class="container">
         <div class="accordion-block__upper">
-            <h5>Subheading</h5>
-            <h2>Sample Title</h2>
+            <?php if($subheading = get_field('subheading')): ?>
+            <h5><?php echo $subheading; ?></h5>
+            <?php endif; ?>
+            <?php if($title = get_field('title')): ?>
+            <h2><?php echo $title; ?></h2>
+            <?php endif; ?>
+            <?php if($description = get_field('description')): ?>
+                <?php echo $description; ?>
+            <?php endif; ?>
         </div>
         <dl class="accordion-block__list">
+            <?php while(have_rows('accordion_item')): the_row(); ?>
             <div class="accordion-block__item">
                 <dt class="accordion-block__headline js-accordion-headline">
                     <h4>
-                        Sample Accordion Headline
+                        <?php echo get_sub_field('headline'); ?>
                         <i class="icon-button-right"></i>
                     </h4>
                 </dt>
                 <dd class="accordion-block__content js-accordion-content">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat lectus et quam ornare, eget imperdiet nulla fermentum. Proin at sodales urna. Duis nec suscipit ipsum. Nulla hendrerit turpis ut leo feugiat fringilla. Pellentesque non dictum massa. In eu risus euismod, auctor diam vel, finibus tortor. Ut dictum urna sed lorem tincidunt sollicitudin. Sed iaculis vestibulum urna, sit amet varius risus egestas et. Suspendisse potenti. Nulla facilisi. Nam auctor tortor nisi, id imperdiet odio blandit id. Etiam vitae accumsan elit.</p>
+                    <?php echo get_sub_field('content'); ?>
                 </dd>
             </div>
-            <div class="accordion-block__item">
-                <dt class="accordion-block__headline js-accordion-headline">
-                    <h4>
-                        Sample Accordion Headline
-                        <i class="icon-button-right"></i>
-                    </h4>
-                </dt>
-                <dd class="accordion-block__content js-accordion-content">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat lectus et quam ornare, eget imperdiet nulla fermentum. Proin at sodales urna. Duis nec suscipit ipsum. Nulla hendrerit turpis ut leo feugiat fringilla. Pellentesque non dictum massa. In eu risus euismod, auctor diam vel, finibus tortor. Ut dictum urna sed lorem tincidunt sollicitudin. Sed iaculis vestibulum urna, sit amet varius risus egestas et. Suspendisse potenti. Nulla facilisi. Nam auctor tortor nisi, id imperdiet odio blandit id. Etiam vitae accumsan elit.</p>
-                </dd>
-            </div>
+            <?php endwhile; ?>
         </dl>
     </div>
 </section>

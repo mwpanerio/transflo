45918b59310252cd0a70dc38f121ccf4e41e9cb8
Mark Win Panerio <mwpanerio@webfx.com>
2022-11-15 11:42:21 +0800

    Update blog pagination

--- wp-content/themes/transflo/single.php
+++ wp-content/themes/transflo/single.php
@@ -16,13 +16,13 @@
                         <?php
                             $prev_post = get_previous_post();
                             if (!empty( $prev_post )): ?>
-                                <a class="btn-post-pagination btn-previous-post" href="<?php echo $prev_post->guid ?>"><strong>Previous Article</strong><span class="post-pagination-text"><?php echo $prev_post->post_title ?></span><span class="mobile-arrow"></span></a>
+                                <a class="btn-post-pagination btn-previous-post" href="<?php echo get_the_permalink($prev_post->ID); ?>"><strong>Previous Article</strong><span class="post-pagination-text"><?php echo $prev_post->post_title ?></span><span class="mobile-arrow"></span></a>
                         <?php endif ?>
 
                         <?php
                             $next_post = get_next_post();
                             if (!empty( $next_post )): ?>
-                            <a class="btn-post-pagination btn-next-post" href="<?php echo $next_post->guid ?>"><strong>Next Article</strong><span class="post-pagination-text"><?php echo $next_post->post_title ?></span><span class="mobile-arrow"></span></a>
+                            <a class="btn-post-pagination btn-next-post" href="<?php echo get_the_permalink($prev_post->ID); ?>"><strong>Next Article</strong><span class="post-pagination-text"><?php echo $next_post->post_title ?></span><span class="mobile-arrow"></span></a>
                         <?php endif ?>
                     </div>
 

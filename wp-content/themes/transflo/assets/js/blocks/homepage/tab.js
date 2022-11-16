f7cb6333b05a4a4a5bcd55a000cec5b2c456274b
Mark Win Panerio <mwpanerio@webfx.com>
2022-11-16 22:48:32 +0800

    Fix tab issues desktop view

--- wp-content/themes/transflo/assets/js/blocks/homepage/tab.js
+++ wp-content/themes/transflo/assets/js/blocks/homepage/tab.js
@@ -8,30 +8,60 @@ var FX = ( function( FX, $ ) {
 		$slider: null,
 
 		init() {
+
+			$('.js-tab-for').on('afterChange', function() {
+				const $this = $(this);
+				const $index = +($this.find('.slick-active').attr('data-slick-index'));
+
+				if($(window).outerWidth() > 768) {
+					$('.js-tab-menu').find('.slick-slide').removeClass('is-current');
+					$('.js-tab-menu').find(`.slick-slide[data-slick-index=${$index}]`).addClass('is-current');
+				}
+			})
+
+			$('.js-tab-menu .tab-menu-item').on('click', function() {
+				const $this = $(this);
+				const $index = +($this.parents('.slick-slide').attr('data-slick-index'));
+
+				if($(window).outerWidth() > 768) {
+					$('.js-tab-for').slick('slickGoTo', $index);
+				}
+			})
+
 			$('.js-tab-for').slick({
 				slidesToShow: 1,
 				slidesToScroll: 1,
 				arrows: false,
 				dots: false,
 				fade: true,
-				asNavFor: '.js-tab-menu',
-				infinite: false,
+				infinite: true,
 				autoplay: true,
 				autoplaySpeed: 2000,
+				responsive: [
+					{
+					  breakpoint: 768,
+					  settings: {
+						autoplay: false,
+						asNavFor: '.js-tab-menu',
+					  }
+					}
+				]
 			});
 
 			$('.js-tab-menu').slick({
 				infinite: true,
-				loop: true,
 				slidesToShow: 3,
-				slidesToScroll: 1,
-				asNavFor: '.js-tab-for',
+				slidesToScroll: 1,				
 				dots: false,
 				arrows: false,
 				responsive: [
 					{
 					  breakpoint: 768,
 					  settings: {
+						focusOnSelect: true,
+						asNavFor: '.js-tab-for',
+						autoplay: true,
+						autoplaySpeed: 2000,
 						slidesToShow: 1,
 						slidesToScroll: 1,
 						variableWidth: true,
--- wp-content/themes/transflo/assets/js/blocks/innerpage/tab-block.js
+++ wp-content/themes/transflo/assets/js/blocks/innerpage/tab-block.js
@@ -14,22 +14,48 @@ var FX = ( function( FX, $ ) {
 				arrows: false,
 				dots: false,
 				fade: true,
-				asNavFor: '.js-tab-menu',
-				infinite: false
-			});
-
-			$('.js-tab-menu').slick({
 				infinite: true,
-				slidesToShow: 4,
-				slidesToScroll: 1,
-				asNavFor: '.js-tab-for',
-				dots: false,
-				arrows: false,
-				focusOnSelect: true,
+				autoplay: true,
+				autoplaySpeed: 2000,
 				responsive: [
 					{
 					  breakpoint: 768,
 					  settings: {
+						autoplay: false,
+						asNavFor: '.js-tab-menu',
+					  }
+					}
+				]
+			});
+
+			$('.js-tab-for').on('afterChange', function() {
+				const $this = $(this);
+				const $index = +($this.find('.slick-active').attr('data-slick-index'));
+
+				if($(window).outerWidth() > 768) {
+					$('.js-tab-menu').find('.slick-slide').removeClass('is-current');
+					$('.js-tab-menu').find(`.slick-slide[data-slick-index=${$index}]`).addClass('is-current');
+				}
+			})
+
+			$('.js-tab-for').on('init', function() {
+				console.log('slick initialize')
+			})
+
+			$('.js-tab-menu').slick({
+				infinite: true,
+				slidesToShow: 4,
+				slidesToScroll: 1,				
+				dots: false,
+				arrows: false,
+				responsive: [
+					{
+					  breakpoint: 768,
+					  settings: {
+						focusOnSelect: true,
+						asNavFor: '.js-tab-for',
+						autoplay: true,
+						autoplaySpeed: 2000,
 						slidesToShow: 1,
 						slidesToScroll: 1,
 						variableWidth: true,

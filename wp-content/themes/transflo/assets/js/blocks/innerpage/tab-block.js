82b068f240d535dd197c6a76350237fc7e41169a
Mark Win Panerio <mwpanerio@webfx.com>
2022-11-17 08:52:33 +0800

    Update tab scripts

--- wp-content/themes/transflo/assets/js/blocks/innerpage/tab-block.js
+++ wp-content/themes/transflo/assets/js/blocks/innerpage/tab-block.js
@@ -8,6 +8,26 @@ var FX = ( function( FX, $ ) {
 		$slider: null,
 
 		init() {
+
+			$('.js-tab-for').on('afterChange', function() {
+				const $this = $(this);
+				const $index = +($this.find('.slick-active').attr('data-slick-index'));
+
+				if($(window).outerWidth() > 768) {
+					$('.js-tab-menu').find('.slick-slide').removeClass('is-current');
+					$('.js-tab-menu').find(`.slick-slide[data-slick-index=${$index}]`).addClass('is-current');
+				}
+			})
+
+			$('.js-tab-menu .tab-menu-item').on('click', function() {
+				const $this = $(this);
+				const $index = +($this.parents('.slick-slide').attr('data-slick-index'));
+
+				if($(window).outerWidth() > 768) {
+					$('.js-tab-for').slick('slickGoTo', $index);
+				}
+			})
+
 			$('.js-tab-for').slick({
 				slidesToShow: 1,
 				slidesToScroll: 1,
@@ -25,16 +45,6 @@ var FX = ( function( FX, $ ) {
 					}
 				]
 			});
-
-			$('.js-tab-for').on('afterChange', function() {
-				const $this = $(this);
-				const $index = +($this.find('.slick-active').attr('data-slick-index'));
-
-				if($(window).outerWidth() > 768) {
-					$('.js-tab-menu').find('.slick-slide').removeClass('is-current');
-					$('.js-tab-menu').find(`.slick-slide[data-slick-index=${$index}]`).addClass('is-current');
-				}
-			})
 
 			$('.js-tab-menu').slick({
 				infinite: true,

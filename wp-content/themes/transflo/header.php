6a3359b2ef9a0c6e43514ddf1adc30dd4ae20c73
Mark Win Panerio <mwpanerio@webfx.com>
2022-11-24 09:52:11 +0800

    Header logo animation update

--- wp-content/themes/transflo/header.php
+++ wp-content/themes/transflo/header.php
@@ -46,7 +46,7 @@
                         EFFICIENT
                     </div>
                     <div class="loader__text__item js-loader-text js-loader-text-logo">
-                        <?php echo fx_get_image_tag( 29281 ); ?>
+                        <?php echo fx_get_image_tag( 29426 ); ?>
                     </div>
                 </div>
             </div>

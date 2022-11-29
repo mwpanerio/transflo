5178b7f01f335f7c4f2897e88bcf2ad9890812f3
Mark Win Panerio <mwpanerio@webfx.com>
2022-11-29 21:11:32 +0800

    Fix benefits section not showing

--- wp-content/themes/transflo/block-templates/innerpage/small-image-button.php
+++ wp-content/themes/transflo/block-templates/innerpage/small-image-button.php
@@ -1,6 +1,6 @@
-<section class="benefits-section benefits-section--version-1 benefits-section--not-slider section-padding <?php echo get_field('background_type'); ?>">
+<section class="js-benefits-section benefits-section benefits-section--version-1 benefits-section--not-slider section-padding <?php echo get_field('background_type'); ?>">
     <div class="container">
-        <?php if(get_field('subheading') || get_field('title') || get_field('description')):?>
+        <?php if(get_field('subheading') || get_field('title') ):?>
         <div class="benefits-section__header">
             <?php if($subheading = get_field('subheading')): ?>
             <h5><?php echo $subheading; ?></h5>
@@ -14,16 +14,18 @@
         </div>
         <?php endif; ?>
         <div class="benefits-section__row">
-            <div class="row flex-row">
+            <div class="row flex-row js-benefits-section-row">
                 <?php while(have_rows('image_button')): the_row(); ?>
-                <div class="benefits-section__item col-xxs-12 col-sm-6 col-lg-4">
+                <div class="benefits-section__item col-xxs-12 col-sm-6 col-lg-4 js-benefits-section-item benefits-section__item">
                     <article class="benefits-section__card">
                         <div class="benefits-section__image">
                             <?php echo fx_get_image_tag(get_sub_field('image')); ?>
                         </div>
+                        <?php if(get_sub_field('description')): ?>
                         <div class="benefits-section__content">
                             <?php echo get_sub_field('description'); ?>
                         </div>
+                        <?php endif; ?>
                     </article>
                 </div>
                 <?php endwhile; ?>
